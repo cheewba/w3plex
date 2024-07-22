9846afa85741e20fe212d9c9f8c7ebf322c984b4
cheewba <chewba34@gmail.com>
2024-07-22 10:11:29 +0200

    Odos rethinking

--- src/w3plex/modules/odos.py
+++ src/w3plex/modules/odos.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from dataclasses import dataclass
-from typing import List, Union, Iterable, Optional
+from typing import List, Union, Iterable, Optional, Tuple
 
 import aiohttp
 from w3ext import Chain, CurrencyAmount, Currency, Account, TokenAmount
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 QUOTE_URL = "https://api.odos.xyz/sor/quote/v2"
 ASSEMBLE_URL = "https://api.odos.xyz/sor/assemble"
+GAS_PRICE_URL = "https://api.odos.xyz/gas/price/{chain_id}"
 EMPTY_WALLET = f"0x{'0' * 40}"
 
 @dataclass
@@ -27,11 +28,15 @@ class Quote:
     price_impact: float
 
     def __str__(self) -> str:
-        input = ", ".join([f"{_in}({self.input_usd[i]})"
+        input_ = ", ".join([f"{_in} ({self.input_usd[i]})"
                            for i, _in in enumerate(self.input)])
-        output = ", ".join([f"{_out}({self.output_usd[i]})"
+        output_ = ", ".join([f"{_out} ({self.output_usd[i]})"
                             for i, _out in enumerate(self.output)])
-        return f"{self.chain}({input} -> {output})"
+        return f"{self.chain}: {input_} -> {output_}"
+
+
+class OdosError(Exception):
+    pass
 
 
 class Odos:
@@ -56,7 +61,7 @@ class Odos:
         return getattr(item, 'address', EMPTY_WALLET)
 
     def _format_output(
-        self, output: Union["Currency", List["Currency"], List[List["Currency", float]]]
+        self, output: Union["Currency", List["Currency"], List[Tuple["Currency", float]]]
     ) -> List[dict]:
         if isinstance(output, Currency):
             output = [[output, 1]]
@@ -72,7 +77,7 @@ class Odos:
         return [{
             "tokenAddress": self._token_address(token),
             "proportion": prop
-        } for [token, prop] in input]
+        } for [token, prop] in output]
 
     def _format_input(
         self, input: Union["CurrencyAmount", List["CurrencyAmount"]]
@@ -80,29 +85,31 @@ class Odos:
         input = input if isinstance(input, Iterable) else [input]
         return [{
             "tokenAddress": self._token_address(item),
-            "amount": item.amount,
+            "amount": str(item.amount),
         } for item in input]
 
     def _format_quote(
         self,
         data: dict,
-        input: List[Currency],
-        output: List[Currency]
+        input_: List[Currency],
+        output_: List[Currency]
     ) -> Quote:
         def sort_tokens(tokens: List[Currency], addresses: List[str]):
-            map = {getattr(t, "address", EMPTY_WALLET).lower() for t in tokens}
+            map = {getattr(t, "address", EMPTY_WALLET).lower(): t for t in tokens}
             return [map[addr.lower()] for addr in addresses]
 
         def to_amounts(tokens: List[Currency], amounts: List[str]):
             return [token.to_amount(int(amounts[i])) for i, token in enumerate(tokens)]
 
-        usd = Currency('USD', 'USD', 2)
+        usd = Currency('USD', 'USD', (usd_decimals := 2))
         return Quote(
             chain=self.chain,
-            input=to_amounts(sort_tokens(input, data["inTokens"]), data['inAmounts']),
-            input_usd=to_amounts([usd] * len(in_usd := data["inValues"]), in_usd),
-            output=to_amounts(sort_tokens(output, data["outTokens"]), data['outAmounts']),
-            output_usd=to_amounts([usd] * len(out_usd := data["outValues"]), out_usd),
+            input=to_amounts(sort_tokens(input_, data["inTokens"]), data['inAmounts']),
+            input_usd=to_amounts([usd] * len(in_usd := data["inValues"]),
+                                 [item * 10 ** usd_decimals for item in in_usd]),
+            output=to_amounts(sort_tokens(output_, data["outTokens"]), data['outAmounts']),
+            output_usd=to_amounts([usd] * len(out_usd := data["outValues"]),
+                                  [item * 10 ** usd_decimals for item in out_usd]),
             gas_limit=data["gasEstimate"],
             price_impact=data["priceImpact"],
             path_id=data["pathId"]
@@ -114,15 +121,7 @@ class Odos:
             "pathId": quote.path_id,
             "simulate": False,
         }
-
-        headers = {
-            "Content-Type": "application/json"
-        }
-        async with self._session as session:
-            async with session.post(ASSEMBLE_URL, data=data, headers=headers) as resp:
-                if not resp.status == 200:
-                    raise ModuleError(f"{self.name}: Assemble swap tx error `{resp.reason}`")
-                return await resp.json()
+        return await self._api_request('post', ASSEMBLE_URL, json=data)
 
     async def _check_permissions(self, input: List["CurrencyAmount"], owner: str, spender: str) -> bool:
         async def get_required(amount: "CurrencyAmount") -> Optional["CurrencyAmount"]:
@@ -139,51 +138,78 @@ class Odos:
             return amount.currency.approve(self.account, spender, amount,
                                            {"nonce": old_nonce})
 
-        need_permissions: List["TokenAmount"] = await asyncio.gather(*[
+        need_permissions: List["TokenAmount"] = list(filter(bool, await asyncio.gather(*[
             get_required(amount) for amount in input
-        ])
+        ])))
         if len(need_permissions) == 0:
             return False
 
-        nonce: int = await self.chain.get_nonce()
+        nonce: int = await self.chain.get_nonce(self.account.address)
         await asyncio.gather(*[approve(amount) for amount in need_permissions])
 
         return True
 
+    async def get_session(self) -> aiohttp.ClientSession:
+        if not self._session:
+            headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+            self._session = aiohttp.ClientSession(headers=headers)
+        return self._session
+
+    async def _api_request(self, method, url, **kwargs):
+        session = await self.get_session()
+        async with session.request(method, url, **kwargs) as resp:
+            if resp.status not in {200, 201}:
+                msg = (f"{self.name}: Api request error to {url} "
+                       f"`{resp.status} - {resp.reason}`")
+                raise OdosError(msg)
+            return await resp.json()
+
     async def get_quote(
         self,
-        input: Union["CurrencyAmount", List["CurrencyAmount"]],
-        output: Union["Currency", List["Currency"], List[List["Currency", float]]],
+        input_: Union["CurrencyAmount", List["CurrencyAmount"]],
+        output_: Union["Currency", List["Currency"], List[Tuple["Currency", float]]],
         slippage: float = 0.5
     ) -> Quote:
+        gas_price = await self._api_request(
+            'get', GAS_PRICE_URL.format(chain_id=self.chain.chain_id)
+        )
         data = {
-            "chainId": self.chain.chain_id,
-            "inputTokens": self._format_input(input),
-            "outputTokens": self._format_output(output),
+            "chainId": int(self.chain.chain_id),
+            "inputTokens": self._format_input(input_),
+            "outputTokens": self._format_output(output_),
             "slippageLimitPercent": slippage,
             "userAddr": self.account.address,
-            "referralCode": self.ref_code or 0,
-            "compact": True
+            "referralCode": self.ref_code or 1,
+            "gasPrice": gas_price["baseFee"],
+            "disableRFQs": False,
+            "likeAsset": True,
+            "compact": True,
+            "pathViz": False,
+            "sourceBlacklist": [],
         }
 
-        headers = {
-            "Content-Type": "application/json"
-        }
-        async with self._session as session:
-            async with session.post(QUOTE_URL, data=data, headers=headers) as resp:
-                if not resp.status == 200:
-                    raise ModuleError(f"{self.name}: Get quote error `{resp.reason}`")
-                return self._format_quote(await resp.json())
+        resp = await self._api_request('post', QUOTE_URL, json=data)
+        return self._format_quote(
+            resp,
+            [item.currency
+                for item in ([input_] if isinstance(input_, CurrencyAmount) else input_)],
+            ([output_] if isinstance(output_, Currency)
+                else [item if isinstance(item, Currency) else item[0] for item in output_])
+        )
 
     async def swap(self, quote: Quote):
-        tx_data = await self._build_swap_tx(quote)
+        tx_data = (await self._build_swap_tx(quote))["transaction"]
 
         if await self._check_permissions(quote.input, self.account.address, tx_data['to']):
             # if we sent some permissions tx's, it might be better to
             # get new transaction data to be sure it's up to date
-            tx_data = await self._build_swap_tx(quote)
+            tx_data = (await self._build_swap_tx(quote))["transaction"]
 
-        tx_hash = self.chain.send_transaction(tx_data, self.account)
+        print(tx_data)
+        tx_hash = await self.chain.send_transaction(tx_data, self.account)
         logger.info(f"{self.name}: Swap transaction sent: {self.chain.get_tx_scan(tx_hash)}")
 
         return await self.chain.wait_for_transaction_receipt(tx_hash)
